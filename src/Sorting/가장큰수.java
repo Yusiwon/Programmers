7ac13afcabc3c245eb17decc6e3f13dac7125ee1
Yusiwon <danseryu@naver.com>
2021-03-22 22:27:33 +0900

    정렬>가장 큰  수 https://programmers.co.kr/learn/courses/30/lessons/42746
    가장 큰 수를 구하기 위해서는 큰 값부터 순서대로 합치면 된다.
    그러나 이때 큰 값은 숫자의 크기로써의 큰 것이 아니라 첫번째자리수부터 큰 것이 큰값이다.
    이를 정렬로써 구현하기 위해서 Arrays.sort를 Comparator를 이용하여 바꾸어 주었다.
    비교문자열을 합친 후 compareTo를 이용하여 return 하여 주면 된다.
    
    그리고 혹시 입력으로 [0, 0, 0]이 들어오게 된다면 이미 정렬이 끝난후의 배열이기때문에 가장 큰값또한 0이될것이다.
    가장 큰값이 "0"일 경우를 조건으로 하여 return 0을 해주었다.

--- src/Sorting/가장큰수.java
+++ src/Sorting/가장큰수.java
@@ -1,14 +1,37 @@
 package Sorting;
 
-public class 가장큰수 {
-    public static void main(String[] args) {
 
-    }
-}
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 
-class Solution {
+class 가장큰수 {
     public String solution(int[] numbers) {
-        String answer = "";
-        return answer;
+
+        String[] temp = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            temp[i] = String.valueOf(numbers[i]);
+        }
+
+        //내림차순으로 정렬
+        Arrays.sort(temp, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        // 입력으로 [0, 0, 0, 0]이 들어오게 되는 경우
+        // 정렬을 마친 배열이므로 가장 큰값이 0인 경우
+        if (temp[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < temp.length;i++) {
+            sb.append(temp[i]);
+        }
+
+        return sb.toString();
     }
 }
