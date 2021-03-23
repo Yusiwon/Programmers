99470ef0a736f86abde6c35f731b71b3d4e61477
Yusiwon <danseryu@naver.com>
2021-03-23 21:52:23 +0900

    완전탐색 > 모의고사 https://programmers.co.kr/learn/courses/30/lessons/42840
    각 사람마다 찍는 답안을 배열에 저장하여 순회하며 비교한다.
    각각 문제를 맞출때마다 count를 해주고 count가 가장 큰 사람을 return한다.
    세 count값을 Math.max를 이용하여 비교하여 ArrayList에 넣어주었다.

---
+++ src/Level1/모의고사.java
@@ -0,0 +1,51 @@
+package Level1;
+
+import java.util.ArrayList;
+
+class 모의고사 {
+    public ArrayList solution(int[] answers) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int acount = 0;
+        int bcount = 0;
+        int ccount = 0;
+
+        int aindex = 0;
+        int bindex = 0;
+        int cindex = 0;
+
+        for (int i = 0; i < answers.length; i++) {
+            acount += answers[i] == a[aindex++] ? 1 : 0;
+            bcount += answers[i] == b[bindex++] ? 1 : 0;
+            ccount += answers[i] == c[cindex++] ? 1 : 0;
+
+            if (aindex == a.length) {
+                aindex = 0;
+            }
+            if (bindex == b.length) {
+                bindex = 0;
+            }
+            if (cindex == c.length) {
+                cindex = 0;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int max = Math.max(acount, bcount);
+        max = Math.max(max, ccount);
+
+        if (max == acount) {
+            list.add(1);
+        }
+        if (max == bcount) {
+            list.add(2);
+        }
+        if (max == ccount) {
+            list.add(3);
+        }
+
+        return list;
+    }
+}
