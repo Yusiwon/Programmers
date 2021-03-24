3c66ee1513e30f6353f16ea9c4a0c1a606c469dd
Yusiwon <danseryu@naver.com>
2021-03-24 17:23:24 +0900

    연습문제 > 휴대폰 번호 가리기 https://programmers.co.kr/learn/courses/30/lessons/12948

---
+++ src/Level1/휴대폰번호가리기.java
@@ -0,0 +1,13 @@
+package Level1;
+
+class  휴대폰번호가리기{
+    public String solution(String phone_number) {
+        String answer = "";
+
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length() - 4);
+        return answer;
+    }
+}
