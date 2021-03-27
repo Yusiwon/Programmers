8cab200e616bc90e73a9aa74cd062b9616299fd4
Yusiwon <danseryu@naver.com>
2021-03-27 18:25:31 +0900

    탐욕법(Greedy) > 체육복 https://programmers.co.kr/learn/courses/30/lessons/42862#
    모든 사람이 기본으로 체육복을 하나씩 가지고 있다는 가정이므로 모든 배열을 1로 채워주고 시작하였다.
    lost배열을 순회하며 체육복을 잃어버린 학생들의 체육복수를 감소시키고
    reserve배열을 순회하면서 여분을 가지고 있는 학생의 체육복수를 증가시켰다.
    그 다음으로 체육복없는 학생에게 나누어주는 연산을 하기위해서 왼쪽또는 오른쪽배열의 체육복 갯수가 2이면 해당체육복수는 1증가시키고 체육복을 준 배열은 1감소 시킨다.
    그러나 이때 가장 왼쪽의 학생은 왼쪽의 학생이 없으므로 오른쪽학생에게만 받을 수 있고 가장 오른쪽 학생은 오른족학생이 없으므로 왼쪽학생에게만 받을 수 있다.
    if문에서 AND연산시에 첫번째 조건을 만족하지 않을 경우 그다음조건은 확인조차 안하고 넘어가는 성질을 이용하여 index확인 && 체육복 수 확인을 해주었다.
    해당 학생의 체육복 수가 0보다 큰 경우 count해준후 return해주었다.

---
+++ src/Level1/체육복.java
@@ -0,0 +1,40 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class 체육복 {
+}
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, 1);
+        arr[0] = 0;
+
+        for (int i = 0; i < lost.length; i++) {
+            arr[lost[i]]--;
+        }
+        for (int i = 0; i < reserve.length; i++) {
+            arr[reserve[i]]++;
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            if (arr[i] == 0) {
+                if (i != 0 && arr[i - 1] == 2) {
+                    arr[i-1]--;
+                    arr[i]++;
+                } else if (i != n && arr[i + 1] == 2) {
+                    arr[i+1]--;
+                    arr[i]++;
+                }
+            }
+
+            if (arr[i] > 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
