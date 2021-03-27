4a648e711be4bea50471175983fbf4267e142810
Yusiwon <danseryu@naver.com>
2021-03-27 17:40:13 +0900

    연습문제 > 문자열 내림차순으로 배치하기 https://programmers.co.kr/learn/courses/30/lessons/12917

--- src/Level1/문자열내림차순으로배치하기.java
+++ src/Level1/문자열내림차순으로배치하기.java
@@ -3,10 +3,7 @@ package Level1;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class 문자열내림차순으로배치하기 {
-}
-
-class Solution {
+class 문자열내림차순으로배치하기 {
     public String solution(String s) {
         String answer = "";
 
