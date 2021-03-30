390dae15a2fdddbe037d83241ac02e214e34126d
Yusiwon <danseryu@naver.com>
2021-03-30 17:30:31 +0900

    찾아라 프로그래밍 마에스터 > 폰켓몬 https://programmers.co.kr/learn/courses/30/lessons/1845
    처음에는 재귀를 이용하여 해결하였다.
    방문여부를 표시할 boolean배열에 방문할때 true한후 count를 해준후 count와 전체크기/2가 될때 뽑기가 된 숫자구성요소의 종류갯수를 구한후 return을 해준다.
    이렇게 하면 원하는 결과가 나오지만 시간초과가 난다
    재귀를 이용하면 모든 경우의 수를 다 거치기때문에 많은 시간이 소요되는것으로 생각된다.
    시간초과를 해결하기위해서는 재귀를 사용하면 안된다.
    
    가장많은 종류의 숫자로 이루어진 경우를 찾아야하는것이라면 주어진 입력에서 중복을 최소화해야한다는것을 깨달았다.
    중복을 제거해주기 위해 HashSet을 이용하여 입력의 중복을 제거해주었다.
    여기에서 조합을 해주면 될까 생각해보았지만 HashSet의 크기가 전체크기/2보다 작다면 가지고 있는 모든 숫자를 이용하고 나머지 공간은 중복이 발생할것이므로 가장 최선의 방법이되므로 HashSet의 크기를 return해주면 된다.
    그렇다면 HashSet의 크기가 전체크기/2보다 크다면 전체크기/2을 return해주면 해결되는 문제이다.

--- src/Level1/폰켓몬.java
+++ src/Level1/폰켓몬.java
@@ -1,43 +1,21 @@
 package Level1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class 폰켓몬 {
 }
 class Solution {
 
-    static boolean[] visit;
-    static int goal;
-    static int max = 0;
-
     public int solution(int[] nums) {
-        int answer = 0;
-        visit = new boolean[nums.length];
-        goal = nums.length / 2;
 
-        recursive(nums.length, 0, nums);
-        return max;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        return set.size() > nums.length/2 ? nums.length/2:set.size();
     }
 
-    public static void recursive(int size, int count, int[] arr) {
-        if (goal == count) {
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int i = 0; i < visit.length; i++) {
-                if (visit[i] && !list.contains(arr[i])) {
-                    list.add(arr[i]);
-                }
-            }
-            if (max < list.size()) {
-                max = list.size();
-            }
-            return;
-        }
-        for (int i = 0; i < size; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                recursive(size, count+1, arr);
-                visit[i] = false;
-            }
-        }
-    }
 }
