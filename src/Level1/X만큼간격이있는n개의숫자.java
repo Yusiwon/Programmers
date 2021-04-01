bf4d075671ce4ee985d40823de601fbb7f0ca246
Yusiwon <danseryu@naver.com>
2021-04-01 22:08:34 +0900

    연습문제 > x만큼 간격이 있는n개의 숫자 https://programmers.co.kr/learn/courses/30/lessons/12954

---
+++ src/Level1/X만큼간격이있는n개의숫자.java
@@ -0,0 +1,13 @@
+package Level1;
+
+class X만큼간격이있는n개의숫자 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long)x * (i+1);
+        }
+        return answer;
+    }
+}
